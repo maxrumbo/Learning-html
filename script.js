3a5e98a860ce05617d7d27a222f994327e22faae
maxrumbo <maxrumbo06@gmail.com>
2025-07-22 13:10:13 +0700

    remove

--- script.js
+++
@@ -1,5 +0,0 @@
-const myButton = document.getElementById('myButton'); // Import tombol dari html
-myButton.addEventListener('click', function(){ // Untuk merespon
-    const heading = document.querySelector('h1'); // Perintah perubahan tombol
-    heading.textContent = 'SUCCESS!';
-});
